5de01dadcd03432c74901bb882de98c39c4b3526
CmdrKien <4437566+KienHui@users.noreply.github.com>
2025-03-05 16:07:19 -0600

    Removed unused things

--- src/main/java/frc/robot/commands/ReefbotAutos.java
+++ src/main/java/frc/robot/commands/ReefbotAutos.java
@@ -1,7 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
